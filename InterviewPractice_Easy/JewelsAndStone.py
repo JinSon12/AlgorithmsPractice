243219a5f30a66bf12d5ebf0cc2a6651c7f53779
Jin <jinson@uw.edu>
2021-05-16 21:32:42 -0700

    minor revision to Jewels and Stones

--- InterviewPractice_Easy/JewelsAndStone.py
+++ InterviewPractice_Easy/JewelsAndStone.py
@@ -1,4 +1,20 @@
 # HashMap
 
-# class Solution:
-#     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+import collections
+
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        # defaultdict initialzed with 0
+        freqs = collections.defaultdict(int)
+        cnt = 0
+
+        for char in stones:
+            freqs[char] += 1
+
+        # for each character in jewels,
+        # add the number of freq. of that char in stones to count.
+        for char in jewels:
+            cnt += freqs[char]
+
+        return cnt
