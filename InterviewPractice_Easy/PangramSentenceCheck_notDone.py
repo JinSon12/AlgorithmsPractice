7e8f0a48206507f5c64c978ccb29a6390143638b
Jin <jinson@uw.edu>
2021-06-14 23:47:15 -0700

    finished pangram easy

--- InterviewPractice_Easy/PangramSentenceCheck_notDone.py
+++ InterviewPractice_Easy/PangramSentenceCheck_notDone.py
@@ -1,2 +1,14 @@
 # 1832. Check if the Sentence Is Pangram
 # https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        alphabet = collections.defaultdict(list)
+
+        for i in sentence:
+            alphabet[i].append(i)
+
+        if (len(alphabet) < 26):
+            return False
+        else:
+            return True
