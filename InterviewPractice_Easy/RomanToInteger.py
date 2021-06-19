9f648f330710cf43af37a2133f859e45b6a6105f
Jin <jinson@uw.edu>
2021-06-18 23:47:39 -0700

    added roman to integer

---
+++ InterviewPractice_Easy/RomanToInteger.py
@@ -0,0 +1,7 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        conversion = {
+            "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000,
+            "IV": 4, "IX": 9, "XL": 50, "XC": 90, ""
+        }
+        print(conversion["I"])
