66067f9799571ac5779a4983732e92b15163b840
Jin <jinson@uw.edu>
2021-07-04 01:01:00 -0700

    JUL 4

--- InterviewPractice_Medium/LC_NextGreatestIntegerIII.py
+++ InterviewPractice_Medium/LC_NextGreatestIntegerIII.py
@@ -20,8 +20,8 @@ class Solution:
         # numbers to the right after we find the firstSmallDig
         nextSmallInd = firstSmallDig + 1
         for ind in range(firstSmallDig + 1, len(numStr)):
-            if ind+1 < len(numStr) and numStr[ind+1] < numStr[nextSmallInd]:
-                nextSmallInd = ind+1
+            if numStr[ind] < numStr[nextSmallInd] and numStr[firstSmallDig] < numStr[ind]:
+                nextSmallInd = ind
                 # print(numStr[nextSmallInd])
 
         numStr[firstSmallDig], numStr[nextSmallInd] = numStr[nextSmallInd], numStr[firstSmallDig]
@@ -32,5 +32,49 @@ class Solution:
         numStr[firstSmallDig+1:] = sorted(numStr[firstSmallDig+1:])
 
         # print(numStr)
+        if int("".join(numStr)) > 2147483647:
+            return -1
+        else:
+            return "".join(numStr)
 
-        return("".join(numStr))
+    def nextGreaterElementV2(self, n: int) -> int:
+        num = list(str(n))
+        ret = -1
+        for i in range(len(num)-1, 0, -1):
+            if num[i] > num[i-1]:
+                for j in range(len(num)-1, i-1, -1):
+                    if num[j] > num[i-1]:
+                        num[j], num[i-1] = num[i-1], num[j]
+                        break
+                ret = int("".join(num[:i]+num[-1:i-1:-1]))
+                if ret > (2**31 - 1):
+                    ret = -1
+                return ret
+        return ret
+
+    def nextGreaterElementV3(self, n: int) -> int:
+        if n <= 11:
+            return -1
+        s = str(n)
+        ln = len(s)
+        fg = 0
+        ind1, ind2 = -1, -1
+        for i in range(ln-1, 0, -1):
+            m = int(s[i])
+            for j in range(i-1, -1, -1):
+                if int(s[j]) < m:
+                    if j > ind1:
+                        ind1 = j
+                        ind2 = i
+                        break
+        if ind1 == -1:
+            return -1
+        ans = list(s)
+        ans[ind1], ans[ind2] = ans[ind2], ans[ind1]
+        # sorting the right half after ind1 because that oart should be in accending order for lowest
+        ans[ind1+1:] = sorted(ans[ind1+1:])
+        st = "".join(ans)
+        an = int(st)
+        if an > (2**31-1):
+            return -1
+        return an
