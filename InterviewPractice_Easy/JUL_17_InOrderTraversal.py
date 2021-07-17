b3a7b0f11c7211de8f16dfbbbecc11031c7115f1
Jin <jinson@uw.edu>
2021-07-17 01:29:20 -0700

    JUL 17

---
+++ InterviewPractice_Easy/JUL_17_InOrderTraversal.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        stack = []
+        curr = root
+        result = []
+
+        # if len(stack) != then True.
+        while(True):
+
+            # until we are at the leftmost node
+            if curr is not None:
+                stack.append(curr)
+                curr = curr.left
+                print(curr)
+                print(stack)
+
+            elif stack:
+                curr = stack.pop()
+                print(curr)
+                result.append(curr.val)
+                curr = curr.right
+            else:
+                break
+
+        return result
