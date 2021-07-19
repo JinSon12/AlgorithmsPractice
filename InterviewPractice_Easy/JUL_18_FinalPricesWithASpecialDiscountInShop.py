3284caa3a99664912de6da850e578b8b47d36e0d
Jin <jinson@uw.edu>
2021-07-18 23:48:58 -0700

    JUL 18

---
+++ InterviewPractice_Easy/JUL_18_FinalPricesWithASpecialDiscountInShop.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+
+""" 
+- use Monotinic Stack. 
+"""
+
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        res = prices[:]
+
+        for i in range(len(prices)):
+
+            while stack and prices[stack[-1]] >= prices[i]:
+                prev_ind = stack.pop()
+                res[prev_ind] = prices[prev_ind] - prices[i]
+
+            stack.append(i)
+
+        return res
--- InterviewPractice_Easy/JUL_18_NextGreaterElement_I.py
+++ InterviewPractice_Easy/JUL_18_NextGreaterElement_I.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/next-greater-element-i/
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack = []
@@ -8,6 +9,7 @@ class Solution:
 #         for el in nums2:
 #             d2[el] = -1
 
+        # O(n) b/c Each element is pushed and popped at most once
         for i in range(len(nums2)):
             while stack and stack[-1] < nums2[i]:
                 d2[(stack.pop())] = nums2[i]
@@ -17,3 +19,16 @@ class Solution:
         # return([d2[x] for x in nums1])
         # use .get(x, -1), to give -1 if the key x doesn't exist in the dictionary.
         return([d2.get(x, -1) for x in nums1])
+
+
+""" 
+MONOTONIC STACK. 
+https://labuladong.gitbook.io/algo-en/ii.-data-structure/monotonicstack
+Further reference on the runtime of the for-while loop: 
+To analyze its time complexity, we need to look at it on a whole: 
+There are n elements in total, each element is pushed into the stack once, 
+and it will be ### pop once at most, without any redundant operation. #### 
+So the total calculation scale is proportional to the element scale n, 
+which is the complexity of O(n).
+
+"""
