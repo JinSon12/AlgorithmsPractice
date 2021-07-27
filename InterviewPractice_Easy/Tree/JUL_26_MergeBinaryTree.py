e49e85722c23800a14780d13a7c90b58f5c2267b
Jin <jinson@uw.edu>
2021-07-26 23:51:10 -0700

    DFS - Merge Binary Tree

---
+++ InterviewPractice_Easy/Tree/JUL_26_MergeBinaryTree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    # solved using DFS - recursion.
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        if root1 == None and root2 != None:
+            return root2
+        elif root1 != None and root2 == None:
+            return root1
+        elif root1 == None and root2 == None:
+            return None
+
+        # print(root1, root2)
+        # Highlight : we are not transferring the saved values to some other nodes,
+        # we just need to change the Tree 1's nodes (by adding the Tree 2's nodes)
+        root1.val = root1.val + root2.val
+
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+
+        return root1
