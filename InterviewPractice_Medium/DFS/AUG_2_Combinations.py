62a3e3ac0cd62c2b042253885978aac775f5fa53
Jin <jinson@uw.edu>
2021-08-02 23:54:27 -0700

    Combinations

---
+++ InterviewPractice_Medium/DFS/AUG_2_Combinations.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/combinations/
+
+
+"""
+77. Combinations
+
+
+"""
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        def dfs(elements, start, k):
+            # 종료 조건. k == 0
+            if k == 0:
+                res.append(elements[:])  # 반드시 슬라이싱으로 카피해주어야지 참조값이 저장되지 않는다.
+
+            for i in range(start, n + 1):
+                elements.append(i)
+                dfs(elements, i + 1, k - 1)
+                elements.pop()
+
+        dfs([], 1, k)
+        return res
