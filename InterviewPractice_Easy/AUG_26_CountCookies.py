594879f7d9670ea309d9987ef1edab9e08291b1a
Jin <jinson@uw.edu>
2021-08-26 23:12:37 -0700

    Assign Cookies

---
+++ InterviewPractice_Easy/AUG_26_CountCookies.py
@@ -0,0 +1,26 @@
+"""
+Assign Cookies. 
+
+Greedy + sorting 
+
+"""
+
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        s.sort()
+        g.sort()
+        cookiePos = 0
+        childP = 0
+        # counter = 0
+
+        while childP < len(g) and cookiePos < len(s):
+            child = g[childP]
+            # print(child, cookiePos, s[cookiePos])
+            if cookiePos < len(s) and child <= s[cookiePos]:
+                # counter += 1 removing this decreases the runtime by half.
+                childP += 1
+
+            cookiePos += 1
+
+        return childP
