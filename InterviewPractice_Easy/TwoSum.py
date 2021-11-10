31195b7999e71801b7e6ea307f75fe8f30db771d
Jin <jinson@uw.edu>
2021-11-09 23:57:08 -0800

    two sum revisited

--- InterviewPractice_Easy/TwoSum.py
+++ InterviewPractice_Easy/TwoSum.py
@@ -13,3 +13,16 @@ class Solution:
                 return [vals[nums[i]], i]
 
             vals[target - nums[i]] = i
+
+# two sum revisited
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # k : target - nums[i], v : ind cur nums[i]
+        dn = {}
+
+        for i in range(len(nums)):
+            if nums[i] in dn:
+                return [i, dn[nums[i]]]
+            dn[target - nums[i]] = i
